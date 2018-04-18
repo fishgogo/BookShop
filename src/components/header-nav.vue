5227e398e66e69daed9b5d2ed5e080d5d4350853
fishgogo <hugangmei@outlook.com>
2018-04-18 16:55:50 +0800

    first commit

---
+++ src/components/header-nav.vue
@@ -0,0 +1,62 @@
+<template>
+	<div id="header">
+		<div class="nav-global">
+			<div class="container">
+				<h1 class="nav-logo">
+					<a href="javascript:;"></a>
+				</h1>
+				<ul class="nav-aside">
+					<li class="nav-user">
+					  <router-link to="/account">个人中心</router-link>
+						<!--active-->
+						<div class="nav-user-wrapper">
+							<div class="nav-user-list">
+								<dl class="nav-user-avatar">
+									<dd><span class="ng-scope"></span></dd>
+									<dt class="ng-binding">+86 158****990</dt>
+								</dl>
+								<ul>
+									<li class="order"><router-link to="/account" exact>我的订单</router-link></li>
+									<!--<li class="support"><a href="javascript:;">售后服务</a></li>
+									<li class="coupon"><a href="javascript:;">我的优惠</a></li>
+									<li class="information"><a href="javascript:;">账户资料</a></li>-->
+									<li class="address"><router-link to="/address">收货地址</router-link></li>
+								<!--	<li class="logout"><a href="javascript:;">退出</a></li>-->
+								</ul>
+							</div>
+						</div>
+					</li>
+					<!--active-->
+					<car-panel class="nav-cart"></car-panel>
+				</ul>
+				<ul class="nav-list">
+					<li><a href="javascript:;">首页</a></li>
+					<li><a href="javascript:;">书城</a></li>
+					<li><a href="javascript:;">售后服务</a></li>
+					<li><a href="javascript:;">联系我们</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="nav-sub">
+			<div class="nav-sub-wrapper">
+				<div class="container">
+					<ul class="nav-list">
+						<li><a href="javascript:;">书们</a></li>
+						<router-link to="/" exact tag="li" activeClass="active">
+						    <a>最新书们</a>
+						</router-link>
+						<li><a href="javascript:;">为你推荐</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+import CarPanel from '@/components/car-panel'
+export default {
+  components: {
+    CarPanel
+  }
+}
+</script>
---
+++ src/lib/newGoodsData.js
@@ -0,0 +1,37 @@
+let goodsData = [{
+  "id": 1000271,
+  "name": "贸易打造的世界",
+  "name_title": "作者:[美]彭慕兰 [美]史蒂文·托皮克 著",
+  "price": 49,
+  "sku_info": [{
+    "sku_id": 100027101,
+    "title": "贸易打造的世界",
+    "sub_title": "作者:[美]彭慕兰 [美]史蒂文·托皮克 著",
+    "ali_image": "http://img3m7.ddimg.cn/17/21/25217297-1_w_3.jpg",
+    "price": 79,
+    "limit_num": 5,
+    "spec_json": {
+      // "image": "attr/v2/1000271/948F029DCF134EF23267994487994DA5/",
+      // "show_name": "红色"
+    }
+  }]
+},{
+  "id": 1000299,
+  "name": "神迹",
+  "name_title": "作者:爱玛·多诺霍，白马时光",
+  "price": 199,
+  "sku_info": [{
+    "sku_id": 100029901,
+    "title": "神迹",
+    "sub_title": "作者:爱玛·多诺霍，白马时光",
+    "ali_image": "http://img3m6.ddimg.cn/9/17/25229466-1_w_6.jpg",
+    "price": 33.6,
+    "limit_num": 1,
+    "spec_json": {
+      // "image": "attr/v2/1000299/B37F37544921114CEF1EC01ED4DF44E4/",
+      // "show_name": "灰色"
+    }
+  }]
+}]
+
+export default goodsData
